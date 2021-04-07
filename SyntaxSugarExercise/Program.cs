8c1499cbb890c839f93befdacaa235b623410a8d
Saba Arefaine <arefainesaba@gmail.com>
2021-04-06 18:48:38 -0700

    inital commit, implemented better syntax

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inferred typing
+            //string interpolation
+            //ternary operator
+
+            //int answer = 4;
+            //string response;
+            //
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + “greater than or equal to nine”;
+            //}
+
+            var answer = 4;
+
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine";
+
+            Console.WriteLine(response);
+        }
+    }
+}
